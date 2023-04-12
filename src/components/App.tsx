bbb2aad8d421ee9d6b26c8c55e1cc32b161b9b1f
solovevayaroslavna <iaroslavna.kozlova@simbirsoft.com>
2023-04-12 18:19:42 +0400

    T-9810776-create-a-React-project: test comment

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React // comment to test git rules
         </a>
       </header>
     </div>
