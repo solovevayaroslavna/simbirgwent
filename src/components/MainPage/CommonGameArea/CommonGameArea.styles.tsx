d7261c60f05621175a5e932e89ee89cd6ea3cd4c
Bogdan <bogda077077@gmail.com>
2023-05-19 00:27:13 +0400

    Т-10404855-NavBar-and-LayoutScalableRefactoring: setting layout

--- src/components/MainPage/CommonGameArea/CommonGameArea.styles.tsx
+++ src/components/MainPage/CommonGameArea/CommonGameArea.styles.tsx
@@ -6,8 +6,8 @@ export const CommonGameAreaWrapper = styled.div`
   grid-template-rows: 4fr 1fr;
   grid-template-columns: 3fr 2fr;
   grid-gap: 0.1vh;
-  height: calc(100vh - 40px);
-  width: 150vh;
+  height: 100%
+  width: 120vh;
   // border: 0.001em solid ${colors.secondaryColor};
   // border-left: 0.001em solid ${colors.secondaryColor};
   background-color: ${colors.commonGameAreaBackgroundColor};
