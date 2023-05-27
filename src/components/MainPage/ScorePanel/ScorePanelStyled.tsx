571652f9d2b6a67f66f9e86f57eefa24f671d581
Denis Podsobilov <dpodsobilov29@gmail.com>
2023-05-27 16:13:30 +0400

    Deleting unnecessary App.tsx, comments

--- src/components/MainPage/ScorePanel/ScorePanelStyled.tsx
+++ src/components/MainPage/ScorePanel/ScorePanelStyled.tsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { colors } from '../../../theme/variables';
 import cardExample from '../../../assets/cards/card-example.png';
 
 export const ScorePanelStyled = styled.div`
@@ -8,11 +7,7 @@ export const ScorePanelStyled = styled.div`
     background-size: 100% 100%;
     display: flex;
     flex-direction: column;
-    // justify-content: ;
-    // align-items: center;
     height: 100%;   
     width: 25%;  
-    // border: 0.001em solid ${colors.secondaryColor};
-    // border-right: 0.001em solid ${colors.secondaryColor};
     float: left;
 `;
