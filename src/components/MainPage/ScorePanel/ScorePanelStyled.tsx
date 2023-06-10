fce0ff2efa47a394e6b3d20bea311ffd78bd1ade
Denis Podsobilov <dpodsobilov29@gmail.com>
2023-06-10 17:09:13 +0400

    deleting comments

--- src/components/MainPage/ScorePanel/ScorePanelStyled.tsx
+++ src/components/MainPage/ScorePanel/ScorePanelStyled.tsx
@@ -1,12 +1,9 @@
 import styled from 'styled-components';
-import { colors } from '../../../theme/variables';
 
 export const ScorePanelStyled = styled.div`
     display: flex;
     flex-direction: column;
     height: 100%;   
     width: 21%;  
-    // border: 0.001em solid ${colors.secondaryColor};
-    // border-right: 0.001em solid ${colors.secondaryColor};
     float: left;
 `;
