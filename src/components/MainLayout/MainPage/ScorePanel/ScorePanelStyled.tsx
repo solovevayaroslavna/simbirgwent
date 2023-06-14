09751095fc4d0a15f31e9b8d27f05d478a25efb0
Bogdan <bogda077077@gmail.com>
2023-06-14 18:12:24 +0400

    T-10754553-remove-float-left-from-frontend-code: replacing deprecated 'float-left' with grid layout

--- src/components/MainLayout/MainPage/ScorePanel/ScorePanelStyled.tsx
+++ src/components/MainLayout/MainPage/ScorePanel/ScorePanelStyled.tsx
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 import cardExample from '../../../../assets/cards/card-example.png';
 
 export const ScorePanelStyled = styled.div`
-    background-image: url(${cardExample});
-    background-repeat: no-repeat;
-    background-size: 100% 100%;
-    height: 100%;   
-    width: 21%;  
-    float: left;
+  display: grid;
+  grid-area: left;
+  height: 100%;
+  background-image: url(${cardExample});
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
