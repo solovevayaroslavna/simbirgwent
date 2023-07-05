5ebe2a83b61c8770eda5bc2083ae95f6b5971f13
Denis Podsobilov <dpodsobilov29@gmail.com>
2023-07-05 22:17:01 +0400

    fixing height of userCard area

--- src/components/MainLayout/MainPage/CommonGameArea/CommonGameArea.styles.tsx
+++ src/components/MainLayout/MainPage/CommonGameArea/CommonGameArea.styles.tsx
@@ -3,7 +3,7 @@ import { colors } from '../../../../theme/variables';
 
 export const CommonGameAreaWrapper = styled.div`
   display: grid;
-  grid-template-rows: 178fr 38fr;
+  grid-template-rows: 178fr 27fr 10fr;
   grid-template-columns: 137fr 56fr;
   height: 100%;
   width: 79%;
--- src/components/MainLayout/MainPage/CommonGameArea/RestOfDeck/RestOfDeckStyled.tsx
+++ src/components/MainLayout/MainPage/CommonGameArea/RestOfDeck/RestOfDeckStyled.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import restOfDeckImage from '../../../../../assets/battlefield/restOfDeck-test.jpeg';
 
 export const RestOfDeckArea = styled.div`
-  grid-row: 2 / 3;
+  grid-row: 2 / 4;
   grid-column: 2 / 3;
   background-image: url(${restOfDeckImage});
   background-size: cover;
